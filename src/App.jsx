8f7c0b0f49f8330ff4f896878313d49dd57a894b
Sudipto Das <eng.sudiptodas@gmail.com>
2026-05-06 22:50:38 +0600

    feat: add official portfolio logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import mainLogo from './assets/mainLogo.png'
 
 const App = () => {
   return (
     <div className='w-screen h-screen flex flex-col items-center justify-center'>
+      <img className='w-20 h-20 object-cover mb-10' src={mainLogo} alt="mainLogo" />
       <h2 className='text-4xl mb-5'>Hello Everyone</h2>
       <h1 className='text-7xl text-gray-700'>I am Sudipto Das</h1>
       <p className='text-xl mt-5'>Web Developer</p>
