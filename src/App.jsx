a75e9b7ad53caa8bccc51d9bcd07e87c66b4cfee
Sudipto Das <eng.sudiptodas@gmail.com>
2026-05-21 00:11:44 +0600

    chore: initialize project structure and configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
-import mainLogo from './assets/mainLogo.png'
+import{BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Skills from './Pages/Skills'
+import Projects from './Pages/Projects'
+import Contact from './Pages/Contact'
+import Details from './Pages/Details'
+
 
 const App = () => {
   return (
-    <div className='w-screen h-screen flex flex-col items-center justify-center'>
-      <img className='w-20 h-20 object-cover mb-10' src={mainLogo} alt="mainLogo" />
-      <h2 className='text-4xl mb-5'>Hello Everyone</h2>
-      <h1 className='text-7xl text-gray-700'>I am Sudipto Das</h1>
-      <p className='text-xl mt-5'>Web Developer</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}  />
+        <Route path="/about" element={<About/>}  />
+        <Route path="/skills" element={<Skills/>}  />
+        <Route path="/projects" element={<Projects/>}  />
+        <Route path="/contact" element={<Contact/>}  />
+        <Route path="/details" element={<Details/>}  />
+      </Routes>
+    </Router>
   )
 }
 
