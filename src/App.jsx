a294acf086b5959e056482d4fdd65712e0c05f55
Sudipto Das <eng.sudiptodas@gmail.com>
2026-06-06 12:59:50 +0600

    fix: set scroll position to top on page refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import{BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './Pages/Home'
 import About from './Pages/About'
@@ -9,6 +9,15 @@ import Details from './Pages/Details'
 
 
 const App = () => {
+
+  // scroll position to top on page refresh 
+  useEffect(() => {
+    if('scrollRestoration' in window.history){
+      window.history.scrollRestoration = 'manual'
+    }
+  },[])
+
+
   return (
     <Router>
       <Routes>
